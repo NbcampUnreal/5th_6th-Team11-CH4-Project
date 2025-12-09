f4c9c9159ea50a3ba1702f12e6309c40573abfd0
hrslayer <hrslayer@naver.com>
2025-12-09 12:20:07 +0900

    초기 언리얼 엔진 파일 및 에셋 업로드

---
+++ Source/BioProtocol/BioProtocol.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BioProtocol.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BioProtocol, "BioProtocol" );
