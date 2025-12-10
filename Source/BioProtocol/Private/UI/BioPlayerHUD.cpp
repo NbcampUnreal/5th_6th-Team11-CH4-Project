01a34c6dd79b6c1fea7add24d59e25222c913e74
hrslayer <hrslayer@naver.com>
2025-12-10 16:05:56 +0900

    [Add] 캐릭터 HUD 클래스 추가 및 테스트용 BP생성 - 캐릭터 체력바 - 캐릭터 역할 - 캐릭터 상호작용 텍스트

---
+++ Source/BioProtocol/Private/UI/BioPlayerHUD.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/BioPlayerHUD.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/Character.h"
+
+void UBioPlayerHUD::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (GetOwningPlayerPawn())
+	{
+		OwnerCharacter = Cast<ACharacter>(GetOwningPlayerPawn());
+	}
+}
+
+void UBioPlayerHUD::UpdateHealth(float CurrentHP)
+{
+	if (HealthBar)
+	{
+		float Percent = FMath::Clamp(CurrentHP / MaxHP, 0.0f, 1.0f);
+		HealthBar->SetPercent(Percent);
+	}
+	if (HealthText)
+	{
+		FText Health = FText::AsNumber((int32)CurrentHP);
+		HealthText->SetText(Health);
+	}
+}
+
+void UBioPlayerHUD::UpdateRoleText(FString NewRole)
+{
+	if (RoleText)
+	{
+		RoleText->SetText(FText::FromString(NewRole));
+	}
+}
---
+++ Source/BioProtocol/Public/UI/BioPlayerHUD.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BioPlayerHUD.generated.h"
+
+class ACharacter;
+class UProgressBar;
+class UTextBlock;
+
+UCLASS()
+class BIOPROTOCOL_API UBioPlayerHUD : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct() override;
+
+	UFUNCTION(BlueprintCallable, Category = "HUD")
+	void UpdateHealth(float CurrentHP);
+
+	UFUNCTION(BlueprintCallable, Category = "HUD")
+	void UpdateRoleText(FString NewRole);
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	UProgressBar* HealthBar;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* HealthText;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* RoleText;
+
+	float MaxHP = 100.0f;
+
+	TWeakObjectPtr<ACharacter> OwnerCharacter;
+};
