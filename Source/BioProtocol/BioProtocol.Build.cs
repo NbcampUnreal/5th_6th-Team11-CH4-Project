135692552419670466cb7fb3291b14880de8ffa7
minsungJun <asrjatk@gmail.com>
2025-12-18 20:48:27 +0900

    fix: build.cs
    voice관련 모듈 추가

--- Source/BioProtocol/BioProtocol.Build.cs
+++ Source/BioProtocol/BioProtocol.Build.cs
@@ -8,7 +8,7 @@ public class BioProtocol : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Slate", "SlateCore", "OnlineSubsystemEOS", "OnlineSubsystemSteam", "OnlineSubsystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Slate", "SlateCore", "OnlineSubsystemEOS", "OnlineSubsystemSteam", "OnlineSubsystem" ,"VoiceChat", "EOSVoiceChat"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
