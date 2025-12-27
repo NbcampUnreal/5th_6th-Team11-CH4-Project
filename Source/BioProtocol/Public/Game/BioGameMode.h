9b6916b27edc62fcc382cf01eb2972945908c381
minsungJun <asrjatk@gmail.com>
2025-12-27 23:10:42 +0900

    Update: BioGameMode, PlayerState
     임시 파일의 내용을 이식

--- Source/BioProtocol/Public/Game/BioGameMode.h
+++ Source/BioProtocol/Public/Game/BioGameMode.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "BioPlayerState.h"
 #include "BioGameMode.generated.h"
 
 class ABioGameState;
@@ -19,6 +20,7 @@ public:
 	ABioGameMode();
 
 	virtual void StartPlay() override;
+	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaSeconds) override;
 	virtual void PostLogin(APlayerController* NewPlayer) override;
 
@@ -30,6 +32,17 @@ public:
 	//biogamemode BP에서 BP_ThirdSpectatorPawn추가 필요
 	UPROPERTY(EditDefaultsOnly, Category = "Spectator")
 	TSubclassOf<AThirdSpectatorPawn> SpectatorPawnClass;
+
+
+
+	// 게임 시작/종료
+	UFUNCTION(BlueprintCallable, Category = "Game")
+	void StartGame();
+
+	UFUNCTION(BlueprintCallable, Category = "Game")
+	void EndGame();
+
+
 
 protected:
 	FVector JailLocation;
@@ -46,4 +59,17 @@ protected:
 
 	UPROPERTY()
 	ABioGameState* BioGameState;
+
+
+	// 보이스 관련
+	void CreateGameVoiceChannels();
+	void CreatePublicGameChannel(const TArray<APlayerController*>& Players);
+	void CreateMafiaGameChannel(const TArray<APlayerController*>& Players);
+	void CreateGameChannel(EBioPlayerRole Team, const TArray<APlayerController*>& Players, const FString& ChannelName);
+
+	FString TrustedServerUrl = TEXT("http://localhost:3000");
+
+	bool bGameVoiceStarted = false;  // 게임 시작 중복 방지
+
+
 };
