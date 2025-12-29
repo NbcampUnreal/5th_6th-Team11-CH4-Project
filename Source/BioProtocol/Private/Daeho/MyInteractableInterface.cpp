68bc31d64b7a889b67153856a9578f36adf372ba
Daeho <84126277+HyukiKR@users.noreply.github.com>
2025-12-29 16:35:02 +0900

    [Add] Simple Task and Item System

---
+++ Source/BioProtocol/Private/Daeho/MyInteractableInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Daeho/MyInteractableInterface.h"
+
+// Add default functionality here for any IMyInteractableInterface functions that are not pure virtual.
