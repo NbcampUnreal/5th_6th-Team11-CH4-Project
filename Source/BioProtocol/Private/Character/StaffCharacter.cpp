034963864bc24278cf67981dd5160592d57a66ac
Byung Kyun Kang <qudrnjs98@naver.com>
2026-01-01 14:48:03 +0900

    feat:죽을때 템 다뺏기

--- Source/BioProtocol/Private/Character/StaffCharacter.cpp
+++ Source/BioProtocol/Private/Character/StaffCharacter.cpp
@@ -397,7 +397,7 @@ void AStaffCharacter::AttackInput(const FInputActionValue& InValue)
 	//UseEquippedItem();
 
 	if (CurrentTool == EToolType::None&&!bIsGunEquipped) {
-		MulticastRPCMeleeAttack();
+		ServerRPCMeleeAttack();
 		return;
 	}
 	if (Ammo > 0 && CurrentTool == EToolType::Gun) {
@@ -903,6 +903,18 @@ float AStaffCharacter::TakeDamage(float DamageAmount, FDamageEvent const& Damage
 	if (Status)
 	{
 		Status->ApplyDamage(ActualDamage, EventInstigator);
+		if (Status->IsDead()) {
+			Client_OnToolBroken();
+			bHasGun = false;
+			Ammo = 0;
+			bHasPotion = false;
+			bHasWrench = false;
+			bHasTorch = false;
+			bIsGunEquipped = false;
+			bIsWrenchEquipped = false;
+			bIsTorchEquipped = false;
+			bIsPotionEquipped = false;
+		}
 	}
 
 	return ActualDamage;
